16f8617e495d8e896192d9151b49667f86e7840c
Raz <razgoldin@gmail.com>
2015-06-28 23:37:38 -0400

    trying new cert generator

--- config/express.js
+++ config/express.js
@@ -145,13 +145,13 @@ module.exports = function(db) {
 		console.log('Securely using https protocol');
 
 		// Load SSL key and certificate
-		// var privateKey = fs.readFileSync('./config/sslcerts/key.pem', 'utf8');
-		// var certificate = fs.readFileSync('./config/sslcerts/cert.pem', 'utf8');
+		var privateKey = fs.readFileSync('./config/sslcerts/key.pem', 'utf8');
+		var certificate = fs.readFileSync('./config/sslcerts/cert.pem', 'utf8');
 
 		// Create HTTPS Server
 		var httpsServer = https.createServer({
-			// key: privateKey,
-			// cert: certificate
+			key: privateKey,
+			cert: certificate
 		}, app);
 
 		// Return HTTPS server instance
